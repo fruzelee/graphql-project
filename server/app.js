fbe81e718fe9a7719c4e4f1ce4223da694a6f015
fruzelee <fazle.rabbi.cse@gmail.com>
2022-11-14 16:53:59 +0600

    creating our very first GraphQL query and RootQueries

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 
+const schema = require('./schema/schema')
+
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    graphiql: true
+    graphiql: true,
+    schema: schema
 }));
 
 
