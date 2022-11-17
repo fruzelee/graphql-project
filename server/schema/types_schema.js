6919723fa76621f56f87030c006aa8bc200f4b97
fruzelee <fazle.rabbi.cse@gmail.com>
2022-11-17 12:14:44 +0600

    add person type with different graphql scalar types

--- server/schema/types_schema.js
+++ server/schema/types_schema.js
@@ -2,15 +2,38 @@ const graphql = require("graphql");
 
 const {
   GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
 } = graphql;
+
+//Scalar Type
+/*
+    String = GraphQLString
+    int 
+    Float
+    Boolean
+    ID
+*/
+
+const Person = new GraphQLObjectType({
+  name: "Person",
+  description: "Represents a Person Type",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    isMarried: { type: GraphQLBoolean },
+    gpa: {type: graphql.GraphQLFloat}
+  }),
+});
 
 //RootQuery
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   description: "Description",
-  fields: {
-
-  },
+  fields: {},
 });
 
 module.exports = new GraphQLSchema({
